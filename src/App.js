c7cac92758f1dc6b598a6464c343dfe2b82328aa
anuzbvbmaniac <anuzbvbmaniac123@gmail.com>
2021-04-29 16:54:24 +0545

    Changes: Initital Init, Componenets, Users, AXIOS, APIs, ENV 1. Initial Init. 2. Components like [Nabbar, UserItem, Users, Spinner]. 3. Axios as API Client. 5. Github API 6. Local Data in .ENV File.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import axios from "axios";
+import './app.css';
+import Users from "./components/users/Users";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        users: [],
+        loading: false,
+    }
+
+    async componentDidMount() {
+        this.setState({ loading: true });
+
+        // axios.get('https://api.github.com/users').then(response => console.log(response.data));
+        const response = await axios.get(`http://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+        this.setState({
+            users: response.data,
+            loading: false,
+        });
+    }
+
+    render() {
+        return (
+            <div className={'App'}>
+                <Navbar title={'Github Finder'} icon={'fab fa-github'}/>
+                <Users loading={this.state.loading} users={this.state.users}/>
+            </div>
+        )
+    }
+
 }
 
 export default App;
